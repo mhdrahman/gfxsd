5379667c6c994ff65ceca586b8db3febeb5898e7
mhdrahman <mhd.rahman601@gmail.com>
2023-12-26 14:47:22 +0000

    Tidy up

--- GFXSD/Controllers/HomeController.cs
+++ GFXSD/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using GFXSD.Models;
 using GFXSD.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
-using static GFXSD.Services.XmlGenerationService;
 
 namespace GFXSD.Controllers
 {
@@ -12,26 +11,18 @@ namespace GFXSD.Controllers
         private readonly XmlGenerationService _xmlGenerationService;
 
         public HomeController(XmlGenerationService xmlGenerationService)
-        {
-            _xmlGenerationService = xmlGenerationService;
-        }
+            => _xmlGenerationService = xmlGenerationService;
 
         [HttpGet]
         public IActionResult Index()
-        {
-            return View();
-        }
+            => View();
 
         [HttpPost]
         public ActionResult GenerateXmlFromSchema([FromBody] Schema schema, [FromQuery] XmlGenerationMode xmlGenerationMode)
-        {
-            return Ok(_xmlGenerationService.GenerateXmlFromSchema(schema.Content, xmlGenerationMode));
-        }
+            => Ok(_xmlGenerationService.GenerateXmlFromSchema(schema.Content, xmlGenerationMode));
 
         [HttpPost]
         public ActionResult RemoveNodes([FromBody] RemoveNodesCommand command)
-        {
-            return Ok(command.Xml.RemoveNodes(command.NodeName));
-        }
+            => Ok(command.Xml.RemoveNodes(command.NodeName));
     }
 }

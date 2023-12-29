447f40706e31dfb064a71f79d6ebae609c270827
mhdrahman <mhd.rahman601@gmail.com>
2023-12-29 20:51:40 +0000

    Changed to linux end line character

--- GFXSD/Extensions/ExceptionUtils.cs
+++ GFXSD/Extensions/ExceptionUtils.cs
@@ -7,7 +7,7 @@ namespace GFXSD.Extensions
 {
     public static class ExceptionUtils
     {
-        private const string Log4j2ErrorMessage = "ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console...\r\n";
+        private const string Log4j2ErrorMessage = "ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console...\n";
 
         public static bool IsLog4j2Error(this string errorMessage)
             => errorMessage.ToLower() == Log4j2ErrorMessage.ToLower();
